65578b75e00819bfed428507bd8c17a16672d0d3
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-12-11 01:50:07 +0600

    Dictionary basice and some methods

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+fruits = {
+    1 : 'Apple',
+    2 : 'Orange',
+    3 : 'Mango',
+    4 : 'Banana',
+    5 : 'Pineapple',
+}
+fruits[5] = 'avocado'#we can change the value of a dictionary
+print(f'the lenght of the fruits is {len(fruits)}')
+print(fruits.keys())
+print(fruits.values())
