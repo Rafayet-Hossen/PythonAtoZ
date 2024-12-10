6e193d6f3ad28a03be0faa8095ae8314925dac55
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-12-11 01:56:23 +0600

    create dictionary using dict and some method

---
+++ DictionaryExtend.py
@@ -0,0 +1,12 @@
+people = dict(
+    john = 30,
+    rob = 45,
+    tim = 20
+)
+people['rafayet'] = 23
+print(people)
+del people['john']
+
+print(people)
+print(people.keys())
+print(people.values())
