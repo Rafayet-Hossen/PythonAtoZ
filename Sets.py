d041773103cbecc41d8c17f3428068aa2f0f0a57
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-12-11 02:22:51 +0600

    Set and the property of set

---
+++ Sets.py
@@ -0,0 +1,11 @@
+numbers = set([1,2,4.5,9])#you can create set like this or
+nums = {1,4,2,4,2}
+print(numbers)
+print(nums)
+
+'''
+Set not contain duplicate values
+Set is stored in ascending manners
+Empty set is a dictionary if you not use set method
+Other wise it will be a set if it contains duplicate values
+'''
